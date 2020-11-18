ece19eb47ea2ccb29b523a1a2efd777d3c27e3aa
Soufiene Slimi <soufiene.slimi@medianet.com.tn>
2020-11-18 16:05:37 +0100

    Allow publishing the migration file

--- src/PConnectorServiceProvider.php
+++ src/PConnectorServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace MedianetDev\PConnector;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class PConnectorServiceProvider extends ServiceProvider
@@ -9,7 +11,7 @@ class PConnectorServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
         /*
          * Optional methods to load your package assets
@@ -23,6 +25,10 @@ class PConnectorServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('p-connector.php'),
             ], 'config');
+
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_p_connector_table.php.stub' => $this->getMigrationFileName($filesystem),
+            ], 'migrations');
 
             // Publishing the views.
             /*$this->publishes([
@@ -59,5 +65,22 @@ class PConnectorServiceProvider extends ServiceProvider
 
             return new PConnector($httpClient);
         });
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_p_connector_table.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_p_connector_table.php")
+            ->first();
     }
 }
